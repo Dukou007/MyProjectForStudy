b4c212d8777507323b08e6b339c097ed1d156ba8
Dukou007 <dukou_caiqin@163.com>
2020-11-11 05:40:55 +0800

    removeduplicate

---
+++ simpletest/src/com/jettech/main/RemoveTheSame.java
@@ -0,0 +1,9 @@
+package com.jettech.main;
+
+/**
+ * @author : dukou_caiqin@163.com
+ * @date: 2020/11/11  5:23
+ * @description:
+ */
+public class RemoveTheSame {
+}
---
+++ springboot_shiro_jsp/src/test/java/com/jettech/SpringbootShiroJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jettech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShiroJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
